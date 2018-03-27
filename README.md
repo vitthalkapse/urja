84793fc51aab3ece38a022967adfe7abf766e877
AMC <amc@AMCs-Mac-mini-2.local>
2018-03-27 20:27:59 +0530

    new branch upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# urja
-urjaInd Website
--- js/main.js
+++ js/main.js
@@ -1,34 +1,173 @@
-$(document).ready(function() { 
-/* code here */ 
+;(function () {
+	
+	'use strict';
 
-	var body = $('#main-con');
-    var backgrounds = [
-      'url(./img/pexels1.jpeg)', 
-      'url(./img/pexels2.jpeg)', 
-      'url(./img/pexels3.jpeg)', 
-      'url(./img/pexels4.jpeg)', 
-      'url(./img/pexels5.jpeg)', 
-      'url(./img/pexels6.jpeg)', 
-      'url(./img/pexels7.jpeg)', 
-      'url(./img/pexels8.jpeg)', 
-      'url(./img/pexels9.jpeg)', 
-      'url(./img/pexels10.jpeg)', 
-      'url(./img/pexels11.jpeg)'];
-    var current = 0;
 
-    function nextBackground() {
-        body.css(
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	// Main Menu Superfish
+	var mainMenu = function() {
+
+		$('#fh5co-primary-menu').superfish({
+			delay: 0,
+			animation: {
+				opacity: 'show'
+			},
+			speed: 'fast',
+			cssArrows: true,
+			disableHI: true
+		});
+
+	};
+
+	// Parallax
+	var parallax = function() {
+		$(window).stellar();
+	};
+
+
+	// Offcanvas and cloning of the main menu
+	var offcanvas = function() {
+
+		var $clone = $('#fh5co-menu-wrap').clone();
+		$clone.attr({
+			'id' : 'offcanvas-menu'
+		});
+		$clone.find('> ul').attr({
+			'class' : '',
+			'id' : ''
+		});
+
+		$('#fh5co-page').prepend($clone);
+
+		// click the burger
+		$('.js-fh5co-nav-toggle').on('click', function(){
+
+			if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			} else {
+				$('body').addClass('fh5co-offcanvas');
+			}
+			// $('body').toggleClass('fh5co-offcanvas');
+
+		});
+
+		$('#offcanvas-menu').css('height', $(window).height());
+
+		$(window).resize(function(){
+			var w = $(window);
+
+
+			$('#offcanvas-menu').css('height', w.height());
+
+			if ( w.width() > 769 ) {
+				if ( $('body').hasClass('fh5co-offcanvas') ) {
+					$('body').removeClass('fh5co-offcanvas');
+				}
+			}
+
+		});	
+
+	}
+
+	
+
+	// Click outside of the Mobile Menu
+	var mobileMenuOutsideClick = function() {
+		$(document).click(function (e) {
+	    var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			}
+	    }
+		});
+	};
+
+
+	// Animations
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp animated');
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '85%' } );
+	};
+
+	// Background Animation
+
+	var cover = $('.fh5co-cover');
+
+	var backgrounds = [
+      'url(./images/cover_bg_1.jpg)', 
+      'url(./images/cover_bg_2.jpg)', 
+      'url(./images/cover_bg_3.jpg)', 
+      'url(./images/cover_bg_4.jpg)', 
+      'url(./images/cover_bg_5.jpg)', 
+      'url(./images/cover_bg_6.jpg)', 
+      'url(./images/cover_bg_7.jpg)', 
+      'url(./images/cover_bg_8.jpg)', 
+      'url(./images/cover_bg_9.jpg)', 
+      'url(./images/cover_bg_10.jpg)', 
+      'url(./images/cover_bg_11.jpg)'];
+
+      var current = 0;
+
+      function nextBackground() {
+
+        cover.css(
             'background',
         backgrounds[current = ++current % backgrounds.length]);
 
-        body.css('background-size', 'cover');
+        cover.css('background-size', 'cover');
 
         setTimeout(nextBackground, 5000);
     }
     setTimeout(nextBackground, 5000);
-    body.css('background', backgrounds[0]);
+    cover.css('background', backgrounds[0]);
+    cover.css('background-size', 'cover');
+	
 
-});
+	// Document on load.
+	$(function(){
+		mainMenu();
+		parallax();
+		offcanvas();
+		mobileMenuOutsideClick();
+		contentWayPoint();
+		
+
+	});
 
 
-	//document.getElementsByClassName('main-container').style.backgroundImage="url(../img/pexels2.jpeg)"; // specify the image path here
+}());
